54294d13e43b7207f0a1e8d60c300ba75152dd59
RobertNolet <r.w.nolet@hva.nl>
2020-12-02 09:54:05 +0100

    AoC Day 2 Solution

--- aoc2/aoc2.py
+++ aoc2/aoc2.py
@@ -8,3 +8,27 @@ Created on Wed Dec  2 09:08:59 2020
 
 import re
 
+# RegExp for the lines in the puzzle input.
+pat = re.compile(r'(\d+)-(\d+) (\w): (\w+)')
+
+# Read data
+data = []
+for line in open('input.txt'):
+    r = pat.match(line)
+    data.append((int(r.group(1)), int(r.group(2)), r.group(3), r.group(4)))
+    
+# Is s a valid password for part 1?
+def valid1(a, b, c, s):
+    return a <= sum(char == c for char in s) <= b
+
+# Is s a valid password for part 2?
+def valid2(a, b, c, s):
+    return sum([s[a-1]==c, s[b-1]==c]) == 1
+
+# Solve part 1
+print(sum(valid1(*t) for t in data))
+
+# Solve part 2
+print(sum(valid2(*t) for t in data))
+
+    
