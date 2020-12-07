04b73c0ff46c4b0c8a8262beb1ee0aa1b1b99493
RobertNolet <r.w.nolet@hva.nl>
2020-12-07 10:10:02 +0100

    AoC Day 7 Solution

--- aoc7/aoc7.py
+++ aoc7/aoc7.py
@@ -10,6 +10,8 @@ import re
 
 pat = re.compile(r'(\d+) (\w+ \w+) bags{0,1}\.{0,1}')
 
+# Load input data as a dictionary. The key is the type of bag, the value is
+# a list of tuples (n, b) where n is the number of bag type b contained.
 bagdata = {}
 for line in open('input.txt'):
     k, v = line.strip().split(' bags contain ')
@@ -19,11 +21,13 @@ for line in open('input.txt'):
             r = pat.match(item)
             bagdata[k].append((int(r.group(1)), r.group(2)))
             
+# Determine whether the bag 'cont' or one of its contents can contain the bag 'bag'
 def canContain(cont, bag):
     if bagdata[cont] == []: return False
     if bag in {b for n, b in bagdata[cont]}: return True
     return any(canContain(b, bag) for n, b in bagdata[cont])
 
+# Determine the number of bags contains in 'bag'
 def numContain(bag):
     if bagdata[bag] == []: return 0
     return sum(n + n*numContain(b) for n,b in bagdata[bag])
