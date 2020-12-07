1ab45f2d542f50a1a044b6b174a1781739271b1c
RobertNolet <r.w.nolet@hva.nl>
2020-12-07 10:49:32 +0100

    AoC Day 7 Solution

--- aoc7/aoc7.py
+++ aoc7/aoc7.py
@@ -12,28 +12,24 @@ pat = re.compile(r'(\d+) (\w+ \w+) bags{0,1}\.{0,1}')
 
 # Load input data as a dictionary. The key is the type of bag, the value is
 # a list of tuples (n, b) where n is the number of bag type b contained.
-bagdata = {}
+data = {}
 for line in open('input.txt'):
     k, v = line.strip().split(' bags contain ')
-    bagdata[k] = []
-    if v != 'no other bags.':
-        for item in v.split(', '):
-            r = pat.match(item)
-            bagdata[k].append((int(r.group(1)), r.group(2)))
+    if v == 'no other bags.':
+        data[k] = []
+    else:
+        data[k] = [[(int(n),b) for n,b in [pat.match(item).groups()]][0] for item in v.split(', ')]
             
 # Determine whether the bag 'cont' or one of its contents can contain the bag 'bag'
 def canContain(cont, bag):
-    if bagdata[cont] == []: return False
-    if bag in {b for n, b in bagdata[cont]}: return True
-    return any(canContain(b, bag) for n, b in bagdata[cont])
+    return bag in {b for n, b in data[cont]} or any(canContain(b, bag) for n, b in data[cont])
 
 # Determine the number of bags contains in 'bag'
 def numContain(bag):
-    if bagdata[bag] == []: return 0
-    return sum(n + n*numContain(b) for n,b in bagdata[bag])
+    return sum(n + n*numContain(b) for n,b in data[bag])
 
 # Part 1
-print(sum(canContain(b, 'shiny gold') for b in bagdata))
+print(sum(canContain(b, 'shiny gold') for b in data))
 
 # Part 2
 print(numContain('shiny gold'))                     
