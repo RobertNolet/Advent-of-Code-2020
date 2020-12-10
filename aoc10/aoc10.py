2814bf681c8b36a49c6ddb84e4ec0ff970920c61
RobertNolet <r.w.nolet@hva.nl>
2020-12-10 11:34:01 +0100

    AoC Day 10 Solution

--- aoc10/aoc10.py
+++ aoc10/aoc10.py
@@ -7,12 +7,13 @@ Created on Thu Dec 10 09:25:03 2020
 """
 
 import numpy as np
+from itertools import groupby
+from functools import reduce
 
-data = np.sort(list(map(int, open('input.txt'))) + [0])
-diff = data[1:] - data[0:-1]
+data = np.diff(np.sort(list(map(int, open('input.txt'))) + [0]))
 
 # Part 1
-print(sum(diff == 1)*(sum(diff == 3)+1))
+print(sum(data == 1)*(sum(data == 3)+1))
 
 # Keep a cache of return value for the count function.
 counts = {0:1}
@@ -21,23 +22,8 @@ counts = {0:1}
 # of values 1, 2, and 3.
 def count(n):
     if n < 0: return 0
-    if n in counts: return counts[n]
-    result = count(n-1) + count(n-2) + count(n-3)
-    counts[n] = result
-    return result
-
-
-n = 0 # Counts subsequences of 1's
-c = 1 # Amount of arrangements
-for d in diff:
-    if d == 1:
-        n += 1
-    elif d == 3:
-        # We just passed a subsequence of n 1 jolt differences. There are
-        # count(n) ways in which these can be arranged in 1,2 or 3 jolt jumps.
-        c = c*count(n) 
-        n = 0 # Reset count
-c = c*count(n) # Include last subsequence of 1's.
+    if n not in counts: counts[n] = count(n-1) + count(n-2) + count(n-3)
+    return counts[n]
 
 # Part 2
-print(c)
+print(reduce(lambda x,y: x*y, [count(len(list(g))) for v, g in groupby(data) if v == 1]))
